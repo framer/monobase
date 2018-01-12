ff6d11681a647464047533e1dfd625789e624637
Koen Bok <koen@framer.com>
2018-01-13 00:36:18 +0100

    Pass the right project

--- examples/default.com/pages/index.tsx
+++ examples/default.com/pages/index.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-
 import { Project } from "monobase";
 
 import Template from "../components/Template";
--- src/page.test.ts
+++
@@ -1,7 +0,0 @@
-import { script } from "./render";
-
-it("should send render", async () => {
-  const result = await script("./pages/test.tsx");
-
-  console.log(result);
-});
