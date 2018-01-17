d5e96af23eef68c6580b4f1ecb9d50c95d886d59
Koen Bok <koen@framer.com>
2018-01-17 23:19:14 +0100

    *** published new version

--- dist/dynamic.js
+++ dist/dynamic.js
@@ -19,19 +19,20 @@ exports.discover = function (dir) {
     var paths = utils.glob(dir + "/**/*.ts{,x}");
     for (var _i = 0, paths_1 = paths; _i < paths_1.length; _i++) {
         var modulePath = paths_1[_i];
+        var importedModule = void 0;
         try {
-            var module_1 = require(modulePath);
+            importedModule = require(modulePath);
         }
         catch (error) {
             continue;
         }
-        for (var _a = 0, _b = Object.keys(module); _a < _b.length; _a++) {
+        for (var _a = 0, _b = Object.keys(importedModule); _a < _b.length; _a++) {
             var key = _b[_a];
-            if (client_1.isDynamicComponent(module[key])) {
+            if (client_1.isDynamicComponent(importedModule[key])) {
                 if (!results[modulePath]) {
                     results[modulePath] = [];
                 }
-                results[modulePath].push(module[key]);
+                results[modulePath].push(importedModule[key]);
             }
         }
     }
