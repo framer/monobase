c7e3216bf985a8dab0fe5fec3503e7b35d46cd9d
Koen Bok <koen@framer.com>
2018-01-19 20:01:28 +0100

    Add nice fading to random image load

--- project/components/examples/Unsplash.tsx
+++ project/components/examples/Unsplash.tsx
@@ -1,12 +1,56 @@
 import * as React from "react";
+import { Dynamic } from "monobase";
 
-export default function Unsplash(props: { width: number; height: number }) {
-  return (
-    <img
-      src={`https://source.unsplash.com/random/${props.width}${props.height}`}
-      style={{ borderRadius: 8 }}
-      width={props.width}
-      height={props.height}
-    />
-  );
+class Unsplash extends React.Component<
+  { width: number; height: number },
+  { loaded: boolean }
+> {
+  state = { loaded: false };
+
+  componentDidMount() {
+    const image = new Image();
+    image.src = this.imageUrl();
+    image.onload = this.onImageLoad;
+  }
+
+  onImageLoad = () => {
+    this.setState({ loaded: true });
+  };
+
+  imageUrl() {
+    const size = `${this.props.width}x${this.props.height}`;
+    return `http://source.unsplash.com/random/${size}`;
+  }
+
+  render() {
+    const base: React.CSSProperties = {
+      display: "inline-block",
+      width: this.props.width,
+      height: this.props.height
+    };
+
+    const wrapperStyle: React.CSSProperties = {
+      ...base,
+      borderRadius: 8,
+      overflow: "hidden",
+      background: "rgb(220, 220, 220)",
+      border: this.state.loaded ? null : "1px solid rgba(0, 0, 0, 0.1)",
+      transition: "border .15s ease-in-out"
+    };
+
+    const imageStyle: React.CSSProperties = {
+      ...base,
+      background: `url('${this.imageUrl()}')`,
+      opacity: this.state.loaded ? 1 : 0,
+      transition: "opacity .15s ease-in-out"
+    };
+
+    return (
+      <div style={wrapperStyle}>
+        <div style={imageStyle} />
+      </div>
+    );
+  }
 }
+
+export default Dynamic(Unsplash);
