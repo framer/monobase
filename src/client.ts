34c550a261d545b360a5356df513d6cae12ca650
Koen Bok <koen@framer.com>
2018-01-27 22:17:45 +0100

    First attempt

--- src/client.ts
+++ src/client.ts
@@ -32,19 +32,62 @@ export const isDynamicComponent = Component => {
 //     : false;
 // }
 
+const getComponents = () => {
+  const components = {};
+
+  for (let i = 0; i < module["i"]; i++) {
+    const modules = __webpack_require__(i);
+
+    for (let key of Object.keys(modules)) {
+      if (isDynamicComponent(modules[key])) {
+        components[modules[key].dynamicName] = modules[key].dynamicComponent;
+      }
+    }
+  }
+
+  return components;
+};
+
+const COMPONENTS = getComponents();
+
+console.log(COMPONENTS);
+
 const hydrate = () => {
   for (let i = 0; i < module["i"]; i++) {
     const modules = __webpack_require__(i);
 
     for (let key of Object.keys(modules)) {
       if (isDynamicComponent(modules[key])) {
-        hydrateComponent(
-          modules[key].dynamicName,
-          modules[key].dynamicComponent
-        );
+        hydrateComponent(modules[key].dynamicName, modules[key]);
       }
     }
   }
+};
+
+const getParents = (element: HTMLElement) => {
+  const elements: HTMLElement[] = [];
+
+  while (element.parentNode) {
+    element = element.parentNode as HTMLElement;
+    elements.push(element);
+  }
+
+  return elements;
+};
+
+const hasParentComponent = (element: HTMLElement) => {
+  for (let parent of getParents(element)) {
+    if (parent.getAttribute && parent.getAttribute("data-component")) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const reactize = child => {
+  const el = COMPONENTS[child.type] || child.type;
+
+  return React.createElement(el, child.props, child.children.map(reactize));
 };
 
 const hydrateComponent = (name, Component) => {
@@ -57,8 +100,13 @@ const hydrateComponent = (name, Component) => {
   }
 
   for (let element of elements) {
+    if (hasParentComponent(element)) {
+      continue;
+    }
+
     const props = JSON.parse(element.getAttribute("data-component-props"));
-    ReactDOM.hydrate(React.createElement(Component, props), element);
+
+    ReactDOM.hydrate(reactize(props), element);
   }
 };
 
--- src/component.tsx
+++ src/component.tsx
@@ -1,18 +1,70 @@
 import * as React from "react";
+import * as ComponentTree from "react-component-tree";
+
+const serializeElement = child => {
+  console.dir(child);
+  return {
+    type: serializeType(child),
+    props: serializeProps(child.props),
+    children: serializeChildren(child.props.children)
+  };
+};
+
+const serializeType = child => {
+  if (child.type && child.type.dynamicName) {
+    return child.type.dynamicName;
+  }
+  if (typeof child.type == "string") {
+    return child.type;
+  }
+  console.dir(child);
+  throw Error(`Unknown type`);
+};
+
+const serializeChildren = children => {
+  console.log("serializeChildren", typeof children, children);
+
+  if (!children) {
+    return [];
+  }
+  if (!Array.isArray(children)) {
+    children = [children];
+  }
+  return children.map(serializeElement);
+};
+
+const serializeProps = props => {
+  if (!props) {
+    return null;
+  }
+
+  const propsCopy = Object.assign({}, props);
+  delete propsCopy.children;
+
+  return propsCopy;
+};
 
 export const Dynamic = Component => {
   const componentName = `Component.${Component.name}`;
 
   // Todo: Maybe try React.createElement("Component")
 
+  console.log("Dynamic", componentName);
+
   const f = props => {
+    const instance = <Component suppressHydrationWarning={true} {...props} />;
+
+    console.dir(serializeElement({ type: componentName, props: props }));
+
     return React.createElement(
       "component",
       {
         "data-component": componentName,
-        "data-component-props": JSON.stringify(props)
+        "data-component-props": JSON.stringify(
+          serializeElement({ type: componentName, props: props })
+        )
       },
-      <Component suppressHydrationWarning={true} {...props} />
+      instance
     );
   };
   f["dynamicName"] = componentName;
