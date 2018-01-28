dd2b595359001495129c1e8f3d722533b5b76b8d
Koen Bok <koen@framer.com>
2018-01-28 01:11:47 +0100

    Put back normal project page

--- project/pages/index.tsx
+++ project/pages/index.tsx
@@ -13,27 +13,62 @@ import Visible from "components/examples/Visible";
 import Picture from "components/examples/Picture";
 import Unsplash from "components/examples/Unsplash";
 
-const Cell = props => {
-  return (
-    <Visible inset={100} {...props}>
-      <Unsplash {...props} />
-    </Visible>
-  );
-};
-
 function render(project) {
   return (
     <Template project={project}>
-      <Example title="Random Image Grid">
-        <Grid
-          width={600}
-          height={1800}
-          columns={2}
-          rows={30}
-          gap={10}
-          cell={Cell}
-        />
-      </Example>
+      <span style={{ textAlign: "center" }}>
+        <section style={{ padding: "60px" }}>
+          <h1>Welcome to Monobase</h1>
+          <p>A simple React based static site generator</p>
+        </section>
+        <Example title="Button">
+          <Button />
+        </Example>
+        <Example title="Mouse Location">
+          <Mouse />
+        </Example>
+        <Example title="Time">
+          <Timer />
+        </Example>
+        <Example title="Enter a color name">
+          <Colors />
+        </Example>
+        <Example title="Persistent Cookie">
+          <Cookie />
+        </Example>
+        <Example title="Visible">
+          <Visible inset={100}>
+            I'm like a refrigerator light. Or Schrödinger's cat.
+          </Visible>
+        </Example>
+        <Example title="CSS Styled">
+          <Styled />
+        </Example>
+        <Example title="Random Image Grid">
+          <Grid
+            width={600}
+            height={300}
+            columns={4}
+            rows={4}
+            gap={10}
+            cell={props => (
+              <Visible>
+                <Unsplash {...props} />
+              </Visible>
+            )}
+          />
+        </Example>
+        <Example title="Picture">
+          <Picture width={400} src="example1.png" alt="Example 1" />
+          <Picture
+            width={400}
+            src="example2.png"
+            alt="Example 2"
+            extensions={["webp"]}
+            hasRetina
+          />
+        </Example>
+      </span>
     </Template>
   );
 }
