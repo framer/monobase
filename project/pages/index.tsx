f7acc05a73a7dad1b0a7cc51d231cb32ee7df390
Koen Bok <koen@framer.com>
2019-12-19 16:33:12 +0100

    Temp template

--- project/pages/index.tsx
+++ project/pages/index.tsx
@@ -11,39 +11,12 @@ import Cookie from "components/examples/Cookie";
 import Visible from "components/examples/Visible";
 import Context from "components/examples/Context";
 
-export default function render() {
+export default function render(project, styles) {
   return (
     <Template>
       <span style={{ textAlign: "center" }}>
-        <section style={{ padding: "60px" }}>
-          <h1>Welcome to Monobase</h1>
-          <p>A simple React based static site generator</p>
-        </section>
         <Example title="Button">
           <Button />
-        </Example>
-        <Example title="Mouse Location">
-          <Mouse />
-        </Example>
-        <Example title="Time">
-          <Timer />
-        </Example>
-        <Example title="CSS Styled Component">
-          <Styled />
-        </Example>
-        <Example title="Visible">
-          <Visible inset={100}>
-            I'm like a refrigerator light. Or Schrödingers cat.
-          </Visible>
-        </Example>
-        <Example title="Enter a color name">
-          <Colors />
-        </Example>
-        <Example title="Persistent Cookie">
-          <Cookie />
-        </Example>
-        <Example title="Contexts">
-          <Context />
         </Example>
       </span>
     </Template>
