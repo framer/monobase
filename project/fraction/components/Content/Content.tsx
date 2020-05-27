f09df9a128a3838887e346db0ecdff00b9911bff
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-05-27 20:53:10 +0200

    Add support for refs and props to Content

--- project/fraction/components/Content/Content.tsx
+++ project/fraction/components/Content/Content.tsx
@@ -1,27 +1,24 @@
 import * as React from "react"
-import { ComponentProps, FC } from "react"
+import { forwardRef, ComponentPropsWithRef, FC } from "react"
 import styles from "./Content.styles.css"
 import cx from "classnames"
 
-export interface Props extends ComponentProps<"div"> {
+export interface Props extends ComponentPropsWithRef<"div"> {
   size?: "small" | "default" | "large" | "larger"
 }
 
-export const Content: FC<Props> = ({
-  children,
-  size = "default",
-  style,
-  className,
-}) => {
-  const classNames = cx(styles.content, className, {
-    [styles.sizeSmall]: size === "small",
-    [styles.sizeLarge]: size === "large",
-    [styles.sizeLarger]: size === "larger",
-  })
+export const Content: FC<Props> = forwardRef(
+  ({ children, size = "default", style, className, ...props }, ref) => {
+    const classNames = cx(styles.content, className, {
+      [styles.sizeSmall]: size === "small",
+      [styles.sizeLarge]: size === "large",
+      [styles.sizeLarger]: size === "larger",
+    })
 
-  return (
-    <div className={classNames} style={style}>
-      {children}
-    </div>
-  )
-}
+    return (
+      <div ref={ref} className={classNames} style={style} {...props}>
+        {children}
+      </div>
+    )
+  }
+)
