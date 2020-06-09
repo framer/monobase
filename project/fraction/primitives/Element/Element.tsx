0d87efe0d8b8a66b447b3945b14467ffdd7da63b
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-06-09 16:43:04 +0200

    Fix Element ref

--- project/fraction/primitives/Element/Element.tsx
+++ project/fraction/primitives/Element/Element.tsx
@@ -7,7 +7,7 @@ export const Element = forwardRef(
   (
     { as: ElementTag = DEFAULT_ELEMENT, ...props }: PrimitivePolymorphicProp,
     ref: Ref<Element>
-  ) => <ElementTag ref={ref} {...props} />
+  ) => <ElementTag {...props} ref={ref} />
 ) as <E extends ElementType = typeof DEFAULT_ELEMENT>(
   props: ElementProps<E>
 ) => JSX.Element
