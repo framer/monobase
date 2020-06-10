e487cf0d714910c54b2707b6a6213f5eb9f63ed8
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-06-10 16:23:16 +0200

    Remove circular dependency

--- project/fraction/tokens/color/index.ts
+++ project/fraction/tokens/color/index.ts
@@ -2,108 +2,6 @@ import { color } from "./light"
 import { colorDark } from "./dark"
 import { createVariables, Scope } from "../utils/variables"
 
-export type Color = {
-  border: string
-  borderHighlight: string
-  toolbarIcon: string
-  translucent: string
-  wireframe: string
-
-  // Page
-  pageBackground: string
-  pageColor: string
-
-  // Navigation
-  navigationBackground: string
-  navigationBorder: string
-  navigationColor: string
-  navigationHamburger: string
-
-  // Button
-  buttonBackground: string
-  buttonColor: string
-  buttonHoverBackground: string
-  buttonFocusOutline: string
-  buttonPrimaryBackground: string
-  buttonPrimaryColor: string
-  buttonPrimaryHoverBackground: string
-  buttonPrimaryFocusOutline: string
-  buttonSecondaryBackground: string
-  buttonSecondaryColor: string
-  buttonSecondaryHoverBackground: string
-  buttonSecondaryFocusOutline: string
-  buttonSuccessBackground: string
-  buttonSuccessColor: string
-  buttonSuccessHoverBackground: string
-  buttonSuccessFocusOutline: string
-  buttonDangerBackground: string
-  buttonDangerColor: string
-  buttonDangerHoverBackground: string
-  buttonDangerFocusOutline: string
-  buttonIconColor: string
-  buttonIconHoverColor: string
-
-  // Footer
-  footerHeading: string
-  footerLink: string
-  footerSecondary: string
-
-  // Input
-  inputBackground: string
-  inputBackgroundTransparent: string
-  inputDisabledColor: string
-  inputHoverBackground: string
-  inputColor: string
-  inputPlaceholderColor: string
-  inputFocusBorder: string
-  inputDangerFocusBorder: string
-  inputDangerColor: string
-
-  // List
-  listActiveBackground: string
-  listColor: string
-  listBorder: string
-
-  // Sheet
-  sheetBackground: string
-  sheetColor: string
-
-  // Tint
-  danger: string
-  dangerDark: string
-  primary: string
-  primaryColor: string
-  primaryDark: string
-  primaryLight: string
-  secondary: string
-  secondaryColor: string
-  secondaryDark: string
-  secondaryLight: string
-  success: string
-  successDark: string
-  tertiary: string
-  vibrant: string
-
-  // Search
-  searchBackground: string
-  selection: string
-  selectionBackground: string
-  selectionBackgroundFaint: string
-  sidebarLink: string
-
-  // Text
-  textHeadline: string
-  textBody: string
-  textDimmed: string
-  textLight: string
-
-  // Tooltip
-  tooltipBackground: string
-
-  // Overlay
-  overlayColor: string
-}
-
 export const [colorVariables, colorValues] = createVariables(color, Scope.Color)
 
 export const [colorVariablesDark, colorValuesDark] = createVariables(
@@ -114,3 +12,4 @@ export const [colorVariablesDark, colorValuesDark] = createVariables(
 export * from "./light"
 export * from "./dark"
 export * from "./palette"
+export * from "./types"
