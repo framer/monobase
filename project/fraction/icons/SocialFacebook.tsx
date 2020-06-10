6bef6746b49c7e8edef7a8789cff85a4a4aea6d8
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-06-10 12:39:14 +0200

    Refactor Icons

--- project/fraction/icons/SocialFacebook.tsx
+++ project/fraction/icons/SocialFacebook.tsx
@@ -1,15 +1,17 @@
 import React from "react"
 import { Component } from "../types"
 
-export const SocialFacebook: Component<"svg"> = ({ width = 24, ...props }) => (
+export const SocialFacebook: Component<"svg"> = ({ width = 20, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="0 0 20 20"
     width={width}
+    role="presentation"
     {...props}
   >
+    <desc>Facebook</desc>
     <path
-      d="M6 12.04A6.02 6.02 0 0112 6c3.31 0 6 2.7 6 6.04 0 3.01-2.2 5.5-5.06 5.96v-4.22h1.4l.26-1.74h-1.66V10.9c0-.47.23-.94.97-.94h.76V8.47s-.69-.11-1.34-.11c-1.37 0-2.27.83-2.27 2.35v1.33H9.54v1.74h1.52V18A6.03 6.03 0 016 12.04z"
+      d="M2 10.053A8.028 8.028 0 0110 2c4.413 0 8 3.6 8 8.053A8.049 8.049 0 0111.253 18v-5.627h1.867l.347-2.32h-2.214v-1.52c0-.626.307-1.253 1.294-1.253h1.013V5.293s-.92-.146-1.787-.146c-1.826 0-3.026 1.106-3.026 3.133v1.773H6.72v2.32h2.027V18A8.04 8.04 0 012 10.053z"
       fill="currentColor"
     />
   </svg>
--- project/fraction/icons/SocialSpectrum.tsx
+++ project/fraction/icons/SocialSpectrum.tsx
@@ -1,15 +1,17 @@
 import React from "react"
 import { Component } from "../types"
 
-export const SocialSpectrum: Component<"svg"> = ({ width = 24, ...props }) => (
+export const SocialSpectrum: Component<"svg"> = ({ width = 20, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="0 0 20 20"
     width={width}
+    role="presentation"
     {...props}
   >
+    <desc>Spectrum</desc>
     <path
-      d="M 6 11 C 6 11.476 6.524 12 7 12 L 7.5 12 C 9.723 12 11.953 13.755 11.953 15.978 L 12 17.5 C 12 17.976 12.524 18 13 18 L 17 18 C 17.476 18 18 17.976 18 17.5 L 18 16.5 C 18 11.101 12.899 6 7.5 6 L 7 6 C 6.524 6 6 6.524 6 7 Z"
+      d="M3 3.933C3 3.418 3.418 3 3.933 3h.817C11.515 3 17 8.485 17 15.25v.817a.933.933 0 01-.933.933h-5.134a.933.933 0 01-.933-.933v-.817A5.25 5.25 0 004.75 10h-.817A.933.933 0 013 9.067z"
       fill="currentColor"
     />
   </svg>
